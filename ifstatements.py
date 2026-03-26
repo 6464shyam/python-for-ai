d0f0e3604a4c99d53a5e5068924f00cfe701f531
shyam-ai-engineer <samprince10ss@gmail.com>
2026-03-26 12:36:08 +0530

    Day1 loops

--- ifstatements.py
+++ ifstatements.py
@@ -3,3 +3,30 @@ if age>=18:
     print("You are eligible to vote.")
 else:
     print("You are not eligible to vote yet.")  
+
+score = 80
+if score >=90:
+    print("Grade: A")
+elif score >=80:
+    print("Grade: B")
+elif score >=70:
+    print("Grade: C")
+else:
+    print("Grade:F")
+
+#single line comment
+"""
+this is a multiline comment """
+
+
+print("Enter your name:")
+name =input()
+print("Hello,"+name+"! ")
+
+print("enter your age:")
+
+age =int(input())
+if age>=18:
+    print("You are an adult.")
+else:
+    print("You are not an adult yet.")
